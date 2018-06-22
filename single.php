dd3db7db02445503a4a9275e4d88e1cb9708c071
Lauren <lmvachon@bu.edu>
2018-06-22 16:05:56 -0400

    styling blog posts

--- single.php
+++ single.php
@@ -2,21 +2,23 @@
 
 <?php get_header(); ?>
 
-<section class="fullPanel intro" data-aos="fade-left">
-  <div class="row">
-    <div class="col-xs-12">
-      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-      <div class="col-sm-12">
-        <h1> <?php the_title(); ?> </h1>
+<div class="blogPostIndiv">
+  <section class="fullPanel" data-aos="fade-left">
+    <div class="row">
+      <div class="col-xs-12">
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <div class="col-sm-12">
+          <h1> <?php the_title(); ?> </h1>
+        </div>
+        <div class="col-sm-12">
+          <?php the_content(); ?>
+        </div>
+        <?php endwhile; else : ?>
+          <p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+        <?php endif; ?>
       </div>
-      <div class="col-sm-12">
-        <?php the_content(); ?>
-      </div>
-      <?php endwhile; else : ?>
-        <p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
-      <?php endif; ?>
     </div>
-  </div>
-</section>
+  </section>
+</div>
 
 <?php get_footer(); ?>
